5dd58f1fcab1ec199200ee2557ac3a78a9cf3108
Aurélien Casteilla <aurelien.casteilla@gmail.com>
2021-12-21 00:44:29 +0100

    main.c added

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * author: Aurélien Casteilla (aurelien.casteilla@gmail.com)
+ *
+ * This file is the entry point of the MajestyPlat game
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char ** argv) {
+    exit(EXIT_SUCCESS);
+}
